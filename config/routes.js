78977b6351f821ea6c0f65bdf2a6998da7f84bab
istvan kosa <istvankosa@istvans-iMac.home>
2021-07-31 18:14:18 -0400

    routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,18 @@
-// TODO: Require Controllers...
+// Require Controllers
+const express = require('express');
+const bodyParser = require('body-parser');
 
 module.exports = (app) => {
-    // TODO...
+	app.get("/", (req, res) => {
+		res.render("index");
+	});
+	app.get("/about", (req, res) => {
+		res.render("about");
+	});
+	app.get("/create", (req, res) => {
+		res.render("create");
+	});
+	app.get("/details/:id", (req, res) => {
+		res.send("detail!");
+	});
 };
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const env = process.env.NODE_ENV || 'development';
-
+const exphbs = require("express-handlebars");
 const config = require('./config/config')[env];
 const app = require('express')();
+const express = require('express');
 
 require('./config/express')(app);
 require('./config/routes')(app);
+app.set("view engine", "hbs");
+app.engine("hbs", exphbs({
+		extname: "hbs",
+		defaultLayout: "index",
+		layoutsDir: __dirname + "/views",
+}));
+
+app.use(express.static("static"));
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
